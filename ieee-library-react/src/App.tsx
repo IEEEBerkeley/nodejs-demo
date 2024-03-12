70024c823480187ea76d14c517c2e882efa61753
goblinrum <ryan.ma3011@berkeley.edu>
2024-03-11 20:22:22 -0700

    Added frontend

--- ieee-library-react/src/App.tsx
+++ ieee-library-react/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>IEEE TechOps Library</h1>
       <BooksGallery />
       </header>
     </div>
